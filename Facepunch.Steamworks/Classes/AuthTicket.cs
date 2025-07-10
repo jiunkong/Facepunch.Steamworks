419ebc30a0083352bf1eac9a214e8ae14c3db897
Jiun Kong <bukgeuk1234@gmail.com>
2025-07-10 15:15:11 +0900

    Update AuthTicket.cs

--- Facepunch.Steamworks/Classes/AuthTicket.cs
+++ Facepunch.Steamworks/Classes/AuthTicket.cs
@@ -5,6 +5,7 @@ namespace Steamworks
 	public class AuthTicket : IDisposable
 	{
 		public byte[] Data;
+  		public int Size;
 		public uint Handle;
 
 		/// <summary>
@@ -19,6 +20,7 @@ namespace Steamworks
 			}
 
 			Handle = 0;
+   			Size = 0;
 			Data = null;
 		}
 
